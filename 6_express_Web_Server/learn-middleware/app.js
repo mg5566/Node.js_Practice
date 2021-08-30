6c671d40ab04d869528c896c34cfd363cb051cb6
강명구 <mg5566@hanmail.net>
2021-08-31 00:12:38 +0900

    update middle-wares

--- 6_express_Web_Server/learn-middleware/app.js
+++ 6_express_Web_Server/learn-middleware/app.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
 
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 /*
 ** middle-ware
 */
+app.use(morgan('dev'));
+// '/' 로 시작하는 요청에 express.static 실행
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: process.env.COOKIE_SECRET,
+	cookie: {
+		httpOnly: true,
+		secure: false,
+	},
+	name: 'session-cookie',
+}));
+
 app.use((req, res, next) => {
 	console.log('모든 요청에 대해 실행됩니다.');
 	next();
